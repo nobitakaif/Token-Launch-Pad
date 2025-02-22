55b5e495e2c0e7b6c814e777b39874816c65b83a
nobitakaif <mk2818356@gmail.com>
2025-02-22 18:50:31 +0000

    initialize the project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { TokenLaunchpad } from './componensts/TokenLaunhPad'
+
+function App() {
+  
+
+  return (
+    <>
+      <TokenLaunchpad/>
+    </>
+  )
+}
+
+export default App
